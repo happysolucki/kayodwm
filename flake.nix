b9597e5248532ac11e8d82bd72c0f1079ee09d69
Brandon Keefer <happysolucki@outlook.com>
2025-10-22 02:03:42 -0500

    init flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "kayotune's DWM build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.dwm.overrideAttrs {
+        src = ./.;
+      };
+    };
+}
