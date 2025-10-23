63e2863c1569a22b92d6fda36f250a2e3c10855c
Brandon Keefer <happysolucki@outlook.com>
2025-10-23 12:56:46 -0500

    add autostart config

--- config.h
+++ config.h
@@ -18,6 +18,11 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"xrandr", "--output", "DP-1", "--mode", "3840x2160", "--rate", "239.99", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
